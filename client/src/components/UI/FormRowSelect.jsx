7f0cdc0977a240df21c2397043d22dd144a8937f
techdave <klatzdavie19@gmail.com>
2022-04-07 07:56:43 +0300

    use createRoot from react18 in index and clear up warnings

--- client/src/components/UI/FormRowSelect.jsx
+++ client/src/components/UI/FormRowSelect.jsx
@@ -7,15 +7,15 @@ const FormRowSelect = ({ labelText, name, value, handleChange, list }) => {
       <label htmlFor='tag' className='task-label'>
         {labelText || name}
       </label>
-      <div class='select-wrapper'>
+      <div className='select-wrapper'>
         <select
-          class='select'
+          className='select'
           name={name}
           value={value}
           onChange={handleChange}>
           {list.map((itemValue, index) => {
             return (
-              <option key={index} value={itemValue} className="select-option">
+              <option key={index} value={itemValue} className='select-option'>
                 {itemValue}
               </option>
             );
